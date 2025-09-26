95cfe0d69705649d0ea73a31aa26ba4db8907507
IgorTorquatto <igor.torquato.20@gmail.com>
2025-09-25 21:27:10 -0300

    feat: adicionar bush e corrigir pontos de crash

--- src/game/tree.h
+++ src/game/tree.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include "ecs/components/model.h"
+
 typedef struct {
     int active;
     float x, y, z;
     float escala;
 } Tree;
 
+typedef struct {
+    int active;
+    float x, y, z;
+    float escala;
+} Bush;
+
 #define MAX_TREES 150
+#define MAX_BUSHES 250
 
 void initTrees();
 void updateTrees(float dt, float worldSpeed);
 void drawTrees();
+
+void initBushes();
+void updateBushes(float dt, float worldSpeed);
+void drawBushes();
